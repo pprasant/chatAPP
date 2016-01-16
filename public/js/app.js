f81c5279e6695281e1e61ae8aaf9daa9e8521a38
Prashant Singh Thakuri <daprasan@outlook.com>
2016-01-16 22:18:28 +1100

    send socket msg back and forth

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connection successfull on the client side !');
 });
+
+socket.on('message', function(message) {
+    console.log('New message:');
+    console.log(message.text);
+})
